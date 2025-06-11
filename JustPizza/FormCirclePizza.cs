39e30b192fe188d5106147c2071f5800f4e3f870
razak.ditp3113 <razak.ditp3113@gmail.com>
2025-06-11 23:40:36 +0800

    modified btnUpdate_Click() method

--- JustPizza/FormCirclePizza.cs
+++ JustPizza/FormCirclePizza.cs
@@ -21,11 +21,9 @@ namespace JustPizza
         {
             InitializeComponent();
 
-
             // Initialize the connection object
             string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\hp\\source\\repos\\JustPizza\\JustPizza\\JustPizzaDatabase.mdf;Integrated Security=True";
             connection = new SqlConnection(connectionString);
-
         }
 
         private void FormCirclePizza_Load(object sender, EventArgs e)
@@ -69,6 +67,28 @@ namespace JustPizza
                 ClearInputs();
             }
 
+        }
+
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (ValidateInputs())
+            {
+                string query = "UPDATE Pizza SET Description = @Description, Dimension = @Dimension, Price = @Price WHERE PizzaID = @PizzaID";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@PizzaID", textBoxCPizzaID.Text);
+                    command.Parameters.AddWithValue("@Description", textBoxCPDescription.Text);
+                    command.Parameters.AddWithValue("@Dimension", textBoxCPDimension.Text);
+                    command.Parameters.AddWithValue("@Price", textBoxCPPrice.Text);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+                LoadData();
+                ClearInputs();
+            }
         }
 
         private bool ValidateInputs()
